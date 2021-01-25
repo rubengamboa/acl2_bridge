a3e4a1fa2131bbcf35fe87c1b788e3dde27f9de6
Ruben Gamboa <rubengamboa@users.noreply.github.com>
2021-01-25 12:50:09 -0700

    Update __init__.py

--- acl2_bridge/__init__.py
+++ acl2_bridge/__init__.py
@@ -1,3 +1,3 @@
-from acl2_bridge.acl2_bridge import ACL2Command, ACL2Bridge
+from acl2_bridge.acl2_bridge import ACL2Command, ACL2Bridge, ACL2BridgeError
 
 __all__ = ["ACL2Bridge", "ACL2Command", "ACL2BridgeError"]
